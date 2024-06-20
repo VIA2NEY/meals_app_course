7a97e33f40231d056fab995e2534969187960fe8
VIA2NEY <kouameangeV11@gmail.com>
2024-06-20 23:22:36 +0000

    Riverpod installing and using

---
+++ lib/providers/meals_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/data/dummy_data.dart';
+
+final mealsprovider = Provider(
+  (ref){
+    return dummyMeals;
+  }
+);
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 const kInitialFilters = {
@@ -14,14 +16,14 @@ const kInitialFilters = {
   Filter.vegen: false,
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<TabsScreen> createState() => _TabsScreenState();
+  ConsumerState<TabsScreen> createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
@@ -79,7 +81,9 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
 
-    final availableMeals = dummyMeals.where((meal) {
+    final meals = ref.watch(mealsprovider);
+
+    final availableMeals = meals.where((meal) {
 
       // Si la valeur de la filterScreen(_selectedFilter) a pour valeur Filter.glutenfree = true 
       // et que l'element de la rechercher .where (meal) n'est pas sans glutten (!meal.isGlutenFree)
